cbf4344e77e5ada44982f182c2fe7f8c5ae6db4f
Endy Muhardin <endy.muhardin@gmail.com>
2014-05-15 17:45:37 +0700

    delete existing record before insert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+delete from m_product
 insert into m_product (code, name, price) values ('P-001', 'Product 001', 15000.00)
 insert into m_product (code, name, price) values ('P-002', 'Product 002', 25000.00)
 insert into m_product (code, name, price) values ('P-003', 'Product 003', 35000.00)
