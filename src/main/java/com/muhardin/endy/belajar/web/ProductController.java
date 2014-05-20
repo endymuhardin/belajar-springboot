2734bc688fa843c31df42250fa9f809818682f41
Endy Muhardin <endy.muhardin@gmail.com>
2014-05-20 17:33:50 +0700

    add prefix api to URL

--- src/main/java/com/muhardin/endy/belajar/web/ProductController.java
+++ src/main/java/com/muhardin/endy/belajar/web/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ProductController {
     
     @Autowired private ProductRepository productRepository;
